85cc904c368e02f493feb06597d16b550a494237
TgPb <vl.makeev1996@gmail.com>
2020-07-17 20:47:58 +0300

    feature: added routes for chat-window and "select or create chat"-window

--- src/pages/ChatsPage/ChatsPage.page.jsx
+++ src/pages/ChatsPage/ChatsPage.page.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
 import styles from './ChatsPage.module.scss';
 
 import ChatsList from "../../components/ChatsList/ChatsList.component";
+import NotFoundPage from "../NotFoundPage/NotFoundPage.page";
 
 const ChatsPage = () => {
+    const { path } = useRouteMatch();
+
     return (
         <section className={styles['chats-page']}>
-            <ChatsList />
+            <Route exact path={[`${path}`, `${path}/:chatId`]}>
+                <ChatsList />
+            </Route>
+            <Switch>
+                <Route exact path={`${path}`} render={()=><div>Please select any chat or create a new one.</div>} />
+                <Route exact path={`${path}/:chatId`} render={({ match: { params: { chatId } } })=><div>Chat { chatId }</div>} />
+                <Route path='*'>
+                    <NotFoundPage />
+                </Route>
+            </Switch>
         </section>
     );
 };
