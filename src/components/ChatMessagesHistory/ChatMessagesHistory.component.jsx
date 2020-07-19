687b31c96e95e876c9588ddc098e8d909303dd5a
TgPb <vl.makeev1996@gmail.com>
2020-07-19 23:37:50 +0300

    feature: added scroll to bottom on mount to chat-messages-history component

--- src/components/ChatMessagesHistory/ChatMessagesHistory.component.jsx
+++ src/components/ChatMessagesHistory/ChatMessagesHistory.component.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useEffect, useRef, Fragment} from 'react';
 
 import styles from './ChatMessagesHistory.module.scss';
 import Message from "../Message/Message.component";
 
 const ChatMessagesHistory = ({ messages }) => {
+    const historyEndRef = useRef(null);
+
+    useEffect(
+        () => {
+            historyEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        },
+        []
+    );
+
     return (
         <div className={styles['chat-messages-history']}>
             {
                 messages && messages.length ? (
-                    messages.map(
-                        (message, index) => <Message key={index} message={message} />
-                    )
+                    <Fragment>
+                        {
+                            messages.map(
+                                (message, index) => <Message key={index} message={message}/>
+                            )
+                        }
+                        <div ref={historyEndRef} />
+                    </Fragment>
                 ) : (
                     <p className={styles['no-messages-warning']}>
                         Chat history is clear.
